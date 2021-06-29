9286c7210a8107409ed4c3c5a8b006b0e00b0ff6
Mostafa Saleh <smostafa@google.com>
2021-06-29 22:05:26 +0000

    KVM: arm64: iommu: Introduce IOMMU driver infrastructure
    To establish DMA isolation, KVM needs an IOMMU driver which provide
    certain ops, these ops are defined outside of the iommu_ops,
    and has 2 components:
    - kvm_iommu_driver (kernel): Implements simple interaction with
      the kernel (init, remove,...)
    - kvm_iommu_ops (hypervisor): Implements identity mapping for
      the IOMMU page tables
    
    Only one driver can be used and is registered with
    kvm_iommu_register_driver() by passing pointers to both ops.
    
    The flow of the KVM IOMMU drivers works as follows:
    - The KVM IOMMU driver in the kernel after being loaded using an
      initcall (before module_init) it would register with KVM both
      the kernel and hypervisor ops.
    - KVM will initialise the driver after it initialises and before the
      de-privilege point, which is a suitable point to establish trusted
      interaction between the host and the hypervisor.
      In this call the IOMMU kernel driver typically starts probing
      the IOMMUs, parsing firmware tables and initialising hardware.
    - Then the hypervisor init will be called, to take over the IOMMUs
      and initialise its data structures.
    
    Signed-off-by: Mostafa Saleh <smostafa@google.com>
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>

--- arch/arm64/kvm/hyp/nvhe/setup.c
+++ arch/arm64/kvm/hyp/nvhe/setup.c
@@ -14,6 +14,7 @@
 #include <nvhe/early_alloc.h>
 #include <nvhe/ffa.h>
 #include <nvhe/gfp.h>
+#include <nvhe/iommu.h>
 #include <nvhe/memory.h>
 #include <nvhe/mem_protect.h>
 #include <nvhe/mm.h>
@@ -389,6 +390,10 @@ void __noreturn __pkvm_init_finalise(void)
 		goto out;
 
 	ret = pin_host_tables();
+	if (ret)
+		goto out;
+
+	ret = kvm_iommu_init();
 	if (ret)
 		goto out;
 
