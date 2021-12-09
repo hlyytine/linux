8a656743633889797e59de9e608caaa1eb4ef4b4
Will Deacon <will@kernel.org>
2021-12-09 13:15:41 +0000

    ANDROID: KVM: arm64: Introduce KVM_CAP_ARM_PROTECTED_VM to set/query PVM firmware
    Expose a new capability, KVM_CAP_ARM_PROTECTED_VM, for protected VMs
    which allows the size of the PVM firmware region to be discovered from
    userspace and for the firmware load address to be specified if it is
    required.
    
    Bug: 357781595
    Change-Id: I819b9b2cfa227f1a0607a8f683aa01d4ae50704f
    Co-developed-by: Fuad Tabba <tabba@google.com>
    Signed-off-by: Fuad Tabba <tabba@google.com>
    Signed-off-by: Will Deacon <will@kernel.org>

--- include/uapi/linux/kvm.h
+++ include/uapi/linux/kvm.h
@@ -969,6 +969,8 @@ struct kvm_enable_cap {
 #define KVM_CAP_RISCV_MP_STATE_RESET 242
 #define KVM_CAP_ARM_CACHEABLE_PFNMAP_SUPPORTED 243
 
+#define KVM_CAP_ARM_PROTECTED_VM 0xffbadab1
+
 struct kvm_irq_routing_irqchip {
 	__u32 irqchip;
 	__u32 pin;
