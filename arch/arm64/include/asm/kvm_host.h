82f655319eb62778788a97ea5b269b43e17042aa
Marc Zyngier <maz@kernel.org>
2022-04-25 14:48:46 +0100

    ANDROID: KVM: arm64: Introduce lazy-ish state sync for non-protected VMs
    Rather than just copying the register state between the hyp and host
    vCPU structures, abstract this code into some helpers which are called
    only for non-protected VMs running under pKVM. To faciliate host access
    to guest registers within a get/put sequence, introduce a new
    'sync_state' hypercall to provide access to the registers of a
    non-protected VM when handling traps.
    
    Bug: 357781595
    Change-Id: I5b0d874d2d2184c4da95a91c0b9b57af500cbce3
    Co-developed-by: Fuad Tabba <tabba@google.com>
    Signed-off-by: Marc Zyngier <maz@kernel.org>
    Signed-off-by: Fuad Tabba <tabba@google.com>

--- arch/arm64/include/asm/kvm_host.h
+++ arch/arm64/include/asm/kvm_host.h
@@ -979,6 +979,10 @@ struct kvm_vcpu_arch {
 /* KVM is currently emulating a nested ERET */
 #define IN_NESTED_ERET		__vcpu_single_flag(sflags, BIT(7))
 
+/* vcpu running in HYP context (VHE-only) */
+#define VCPU_HYP_CONTEXT	__vcpu_single_flag(iflags, BIT(7))
+/* pKVM host vcpu state is dirty, needs resync (nVHE-only) */
+#define PKVM_HOST_STATE_DIRTY	__vcpu_single_flag(iflags, BIT(7))
 
 /* Pointer to the vcpu's SVE FFR for sve_{save,load}_state() */
 #define vcpu_sve_pffr(vcpu) (kern_hyp_va((vcpu)->arch.sve_state) +	\
