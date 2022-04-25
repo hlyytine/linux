7bce9386ad735f423e36a1cd3852ca9a74639541
Will Deacon <will@kernel.org>
2022-04-25 15:20:12 +0000

    ANDROID: KVM: arm64: Introduce KVM_VM_TYPE_ARM_PROTECTED machine type for PVMs
    Introduce a new virtual machine type, KVM_VM_TYPE_ARM_PROTECTED, which
    specifies that the guest memory pages are to be unmapped from the host
    stage-2 by the hypervisor.
    
    Bug: 357781595
    Change-Id: Iabcd03865aed4a41637597ac247897fd185bfc4d
    Signed-off-by: Will Deacon <will@kernel.org>

--- arch/arm64/kvm/arm.c
+++ arch/arm64/kvm/arm.c
@@ -154,6 +154,9 @@ int kvm_arch_init_vm(struct kvm *kvm, unsigned long type)
 {
 	int ret;
 
+	if (type & ~KVM_VM_TYPE_MASK)
+		return -EINVAL;
+
 	mutex_init(&kvm->arch.config_lock);
 
 #ifdef CONFIG_LOCKDEP
@@ -185,7 +188,7 @@ int kvm_arch_init_vm(struct kvm *kvm, unsigned long type)
 		 * If any failures occur after this is successful, make sure to
 		 * call __pkvm_unreserve_vm to unreserve the VM in hyp.
 		 */
-		ret = pkvm_init_host_vm(kvm);
+		ret = pkvm_init_host_vm(kvm, type);
 		if (ret)
 			goto err_free_cpumask;
 	}
