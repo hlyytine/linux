7df7bb13c67fc2130352a35a5f4fc5c3a9b5167c
Vincent Donnefort <vdonnefort@google.com>
2023-05-12 11:32:57 +0100

    KVM: arm64: Introducing pkvm_mem_abort_range()
    Previously, guest stage-2 mapping was only triggered on a guest fault.
    If the hypervisor needs one (e.g to share memory between guest and
    host) its only solution is to fake a fault and to return to host. Of
    course being done on a per-page basis, it is quite inefficient.
    
    Instead provide a function to map an entire range for a guest stage-2
    which will later be used to satisfy hypervisor mapping requests.
    
    Even if a range is passed, this function doesn't allow the creation of
    block mappings.
    
    Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

--- arch/arm64/kvm/mmu.c
+++ arch/arm64/kvm/mmu.c
@@ -1437,16 +1437,50 @@ static int pkvm_host_map_guest(u64 pfn, u64 gfn)
 	return (ret == -EPERM) ? -EAGAIN : ret;
 }
 
+#define node_ppage(__node) \
+	(container_of(__node, struct kvm_pinned_page, node))
+
+static int cmp_ppage(const void *key, const struct rb_node *node)
+{
+	struct kvm_pinned_page *ppage = node_ppage(node);
+	phys_addr_t ipa = (phys_addr_t)key;
+
+	if (ppage->ipa < ipa)
+		return 1;
+	else if (ppage->ipa > ipa)
+		return -1;
+
+	return 0;
+}
+
 static int cmp_ppages(struct rb_node *node, const struct rb_node *parent)
 {
-	struct kvm_pinned_page *a = container_of(node, struct kvm_pinned_page, node);
-	struct kvm_pinned_page *b = container_of(parent, struct kvm_pinned_page, node);
+	return cmp_ppage((void *)(node_ppage(node))->ipa, parent);
+}
 
-	if (a->ipa < b->ipa)
-		return -1;
-	if (a->ipa > b->ipa)
-		return 1;
-	return 0;
+static inline struct kvm_pinned_page *
+find_ppage_or_above(struct kvm *kvm, phys_addr_t ipa)
+{
+	struct rb_node *node = kvm->arch.pkvm.pinned_pages.rb_node;
+
+	while (node) {
+		int ret = cmp_ppage((void *)ipa, node);
+
+		if (!ret) {
+			break;
+		} else if (ret > 0) {
+			node = node->rb_right;
+		} else if (ret < 0) {
+			if (!node->rb_left)
+				break;
+			node = node->rb_left;
+		}
+	}
+
+	if (!node)
+		return NULL;
+
+	return node_ppage(node);
 }
 
 static int insert_ppage(struct kvm *kvm, struct kvm_pinned_page *ppage)
@@ -1537,6 +1571,53 @@ free_ppage:
 	kfree(ppage);
 
 	return ret;
+}
+
+int pkvm_mem_abort_range(struct kvm_vcpu *vcpu, phys_addr_t fault_ipa, size_t size)
+{
+	phys_addr_t ipa_end = fault_ipa + size - 1;
+	struct kvm_pinned_page *ppage;
+	int err = 0, idx;
+
+	if (!PAGE_ALIGNED(size) || !PAGE_ALIGNED(fault_ipa))
+		return -EINVAL;
+
+	if (ipa_end >= BIT_ULL(get_kvm_ipa_limit()) ||
+	    ipa_end >= kvm_phys_size(vcpu->kvm))
+		return -EINVAL;
+
+	idx = srcu_read_lock(&vcpu->kvm->srcu);
+
+	ppage = find_ppage_or_above(vcpu->kvm, fault_ipa);
+
+	while (size) {
+		gfn_t gfn = fault_ipa >> PAGE_SHIFT;
+		struct kvm_memory_slot *memslot;
+		unsigned long hva;
+		bool writable;
+
+		if (ppage && ppage->ipa == fault_ipa) {
+			ppage = node_ppage(rb_next(&ppage->node));
+		} else {
+			memslot = gfn_to_memslot(vcpu->kvm, gfn);
+			hva = gfn_to_hva_memslot_prot(memslot, gfn, &writable);
+			if (kvm_is_error_hva(hva) || !writable) {
+				err = -EINVAL;
+				goto end;
+			}
+
+			err = pkvm_mem_abort(vcpu, fault_ipa, hva);
+			if (err)
+				goto end;
+		}
+
+		size -= PAGE_SIZE;
+		fault_ipa += PAGE_SIZE;
+	}
+end:
+	srcu_read_unlock(&vcpu->kvm->srcu, idx);
+
+	return err;
 }
 
 static int user_mem_abort(struct kvm_vcpu *vcpu, phys_addr_t fault_ipa,
