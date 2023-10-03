ebfc56c15e8c79d141a2083e50afbb3515881961
Mostafa Saleh <smostafa@google.com>
2023-10-03 14:52:18 +0000

    ANDROID: KVM: arm64: Add unified interface to topup/reclaim hyp allocators
    Currently, there is one interface (3 HVCs) to deal with the hyp(heap)
    allocator. In preparation to rework the IOMMU memory donation,
    we can reuse the same infrastructure as the hyp allocator.
    As the IOMMU page table pages are allocated from different allocator,
    we need a new abstraction for allocators in the hypervisor that the
    HVCs can use.
    
    Bug: 277989609
    Bug: 278749606
    Bug: 357781595
    Change-Id: I7a96e637f535832b3580bfc96aeb670b6a9d1a25
    Signed-off-by: Mostafa Saleh <smostafa@google.com>

--- arch/arm64/include/asm/kvm_host.h
+++ arch/arm64/include/asm/kvm_host.h
@@ -1637,4 +1637,9 @@ void kvm_set_vm_id_reg(struct kvm *kvm, u32 reg, u64 val);
 #define kvm_has_s1poe(k)				\
 	(kvm_has_feat((k), ID_AA64MMFR3_EL1, S1POE, IMP))
 
+/* Allocator interface IDs. */
+#define HYP_ALLOC_MGT_HEAP_ID		0
+
+unsigned long __pkvm_reclaim_hyp_alloc_mgt(unsigned long nr_pages);
+
 #endif /* __ARM64_KVM_HOST_H__ */
