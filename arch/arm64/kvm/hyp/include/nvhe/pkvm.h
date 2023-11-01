bzy-080408/android_pkvm_kernel

KVM: arm64: Reset primary vCPU according to PVM firmware boot protocol
When a PVM firmware image is present for a protected VM, treat the first
running vCPU as the "primary" vCPU and reset its registers accordingly,
in particular by initialising its PC to enter the firmware at startup.

Signed-off-by: Will Deacon <will@kernel.org>

arch/arm64/kvm/hyp/include/nvhe/pkvm.h
@@ -33,6 +33,9 @@ struct kvm_shadow_vm {
 	/* Pointers to the shadow vcpus of the shadow vm. */
 	struct kvm_vcpu *vcpus[KVM_MAX_VCPUS];
 
+	/* Primary vCPU pending entry to the pvmfw */
+	struct kvm_vcpu *pvmfw_entry_vcpu;
+
 	/* The host's kvm structure. */
 	struct kvm *host_kvm;
 

hlyytine/linux

arch/arm64/kvm/hyp/include/nvhe/pkvm.h
@@ -55,6 +55,9 @@ struct pkvm_hyp_vm {
 	struct hyp_pool pool;
 	hyp_spinlock_t pgtable_lock;
 
+	/* Primary vCPU pending entry to the pvmfw */
+	struct pkvm_hyp_vcpu *pvmfw_entry_vcpu;
+
 	/*
 	 * The number of vcpus initialized and ready to run.
 	 */