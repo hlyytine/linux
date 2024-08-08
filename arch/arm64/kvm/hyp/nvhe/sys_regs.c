BayLibre/android_kernel_common

ANDROID: KVM: arm64: Consolidate allowed and restricted guest cpu feature checks
The definitions for allowed and restricted cpu features for
protected guests, which are based on feature registers were
defined and checked for separately. This resulted in an
inconsistency when checking if pointer authentication is allowed,
which resulted in trapping it for protected guests despite it
being allowed.

To avoid this happening in the future, consolidate the
definitions into one. Use that new definition to construct the
guest view of the feature registers for consistency.

Bug: 278749606
Change-Id: Ia290b98a44ca533578add5ab4ca06888eb62e507
Fixes: 6c30bfb18d0b ("KVM: arm64: Add handlers for protected VM System Registers")
Reported-by: Mostafa Saleh <smostafa@google.com>
Signed-off-by: Fuad Tabba <tabba@google.com>

arch/arm64/kvm/hyp/nvhe/sys_regs.c
@@ -53,7 +53,6 @@ static void inject_undef64(struct kvm_vcpu *vcpu)
 /*
  * Returns the restricted features values of the feature register based on the
  * limitations in restrict_fields.
- * A feature id field value of 0b0000 does not impose any restrictions.
  * Note: Use only for unsigned feature field values.
  */
 static u64 get_restricted_features_unsigned(u64 sys_reg_val,
@@ -69,10 +68,9 @@ static u64 get_restricted_features_unsigned(u64 sys_reg_val,
 	 * unsigned value between the one supported by the system, and what the
 	 * value is being restricted to.
 	 */
-	while (sys_reg_val && restrict_fields) {
+	while (sys_reg_val) {
 		value |= min(sys_reg_val & mask, restrict_fields & mask);
 		sys_reg_val &= ~mask;
-		restrict_fields &= ~mask;
 		mask <<= ARM64_FEATURE_FIELD_BITS;
 	}
 
@@ -86,33 +84,32 @@ static u64 get_restricted_features_unsigned(u64 sys_reg_val,
 
 static u64 get_pvm_id_aa64pfr0(const struct kvm_vcpu *vcpu)
 {
-	u64 set_mask = 0;
-	u64 allow_mask = PVM_ID_AA64PFR0_ALLOW;
-
-	set_mask |= get_restricted_features_unsigned(id_aa64pfr0_el1_sys_val,
-		PVM_ID_AA64PFR0_RESTRICT_UNSIGNED);
+	u64 value = get_restricted_features_unsigned(id_aa64pfr0_el1_sys_val,
+						     PVM_ID_AA64PFR0_ALLOW);
 
 	if (!vcpu_has_sve(vcpu))
-		set_mask &= ~ARM64_FEATURE_MASK(ID_AA64PFR0_EL1_SVE);
+		value &= ~ARM64_FEATURE_MASK(ID_AA64PFR0_EL1_SVE);
 
-	return (id_aa64pfr0_el1_sys_val & allow_mask) | set_mask;
+	return value;
 }
 
 static u64 get_pvm_id_aa64pfr1(const struct kvm_vcpu *vcpu)
 {
 	const struct kvm *kvm = (const struct kvm *)kern_hyp_va(vcpu->kvm);
-	u64 allow_mask = PVM_ID_AA64PFR1_ALLOW;
+	u64 value = get_restricted_features_unsigned(id_aa64pfr1_el1_sys_val,
+						     PVM_ID_AA64PFR1_ALLOW);
 
 	if (!kvm_has_mte(kvm))
-		allow_mask &= ~ARM64_FEATURE_MASK(ID_AA64PFR1_EL1_MTE);
+		value &= ~ARM64_FEATURE_MASK(ID_AA64PFR1_EL1_MTE);
 
-	return id_aa64pfr1_el1_sys_val & allow_mask;
+	return value;
 }
 
 static u64 get_pvm_id_aa64zfr0(const struct kvm_vcpu *vcpu)
 {
 	if (vcpu_has_sve(vcpu))
-		return id_aa64zfr0_el1_sys_val & PVM_ID_AA64ZFR0_ALLOW;
+		return get_restricted_features_unsigned(id_aa64zfr0_el1_sys_val,
+							PVM_ID_AA64ZFR0_ALLOW);
 
 	return 0;
 }
@@ -164,46 +161,46 @@ static u64 get_pvm_id_aa64isar0(const struct kvm_vcpu *vcpu)
 
 static u64 get_pvm_id_aa64isar1(const struct kvm_vcpu *vcpu)
 {
-	u64 allow_mask = PVM_ID_AA64ISAR1_ALLOW;
+	u64 value = get_restricted_features_unsigned(id_aa64isar1_el1_sys_val,
+						     PVM_ID_AA64ISAR1_ALLOW);
 
 	if (!vcpu_has_ptrauth(vcpu))
-		allow_mask &= ~(ARM64_FEATURE_MASK(ID_AA64ISAR1_EL1_APA) |
-				ARM64_FEATURE_MASK(ID_AA64ISAR1_EL1_API) |
-				ARM64_FEATURE_MASK(ID_AA64ISAR1_EL1_GPA) |
-				ARM64_FEATURE_MASK(ID_AA64ISAR1_EL1_GPI));
+		value &= ~(ARM64_FEATURE_MASK(ID_AA64ISAR1_EL1_APA) |
+			   ARM64_FEATURE_MASK(ID_AA64ISAR1_EL1_API) |
+			   ARM64_FEATURE_MASK(ID_AA64ISAR1_EL1_GPA) |
+			   ARM64_FEATURE_MASK(ID_AA64ISAR1_EL1_GPI));
 
-	return id_aa64isar1_el1_sys_val & allow_mask;
+	return value;
 }
 
 static u64 get_pvm_id_aa64isar2(const struct kvm_vcpu *vcpu)
 {
-	u64 allow_mask = PVM_ID_AA64ISAR2_ALLOW;
+	u64 value = get_restricted_features_unsigned(id_aa64isar2_el1_sys_val,
+						     PVM_ID_AA64ISAR2_ALLOW);
 
 	if (!vcpu_has_ptrauth(vcpu))
-		allow_mask &= ~(ARM64_FEATURE_MASK(ID_AA64ISAR2_EL1_APA3) |
-				ARM64_FEATURE_MASK(ID_AA64ISAR2_EL1_GPA3));
+		value &= ~(ARM64_FEATURE_MASK(ID_AA64ISAR2_EL1_APA3) |
+			   ARM64_FEATURE_MASK(ID_AA64ISAR2_EL1_GPA3));
 
-	return id_aa64isar2_el1_sys_val & allow_mask;
+	return id_aa64isar2_el1_sys_val & value;
 }
 
 static u64 get_pvm_id_aa64mmfr0(const struct kvm_vcpu *vcpu)
 {
-	u64 set_mask;
-
-	set_mask = get_restricted_features_unsigned(id_aa64mmfr0_el1_sys_val,
-		PVM_ID_AA64MMFR0_RESTRICT_UNSIGNED);
-
-	return (id_aa64mmfr0_el1_sys_val & PVM_ID_AA64MMFR0_ALLOW) | set_mask;
+	return get_restricted_features_unsigned(id_aa64mmfr0_el1_sys_val,
+						PVM_ID_AA64MMFR0_ALLOW);
 }
 
 static u64 get_pvm_id_aa64mmfr1(const struct kvm_vcpu *vcpu)
 {
-	return id_aa64mmfr1_el1_sys_val & PVM_ID_AA64MMFR1_ALLOW;
+	return get_restricted_features_unsigned(id_aa64mmfr1_el1_sys_val,
+						PVM_ID_AA64MMFR1_ALLOW);
 }
 
 static u64 get_pvm_id_aa64mmfr2(const struct kvm_vcpu *vcpu)
 {
-	return id_aa64mmfr2_el1_sys_val & PVM_ID_AA64MMFR2_ALLOW;
+	return get_restricted_features_unsigned(id_aa64mmfr2_el1_sys_val,
+						PVM_ID_AA64MMFR2_ALLOW);
 }
 
 /* Read a sanitized cpufeature ID register by its encoding */
@@ -278,7 +275,7 @@ static bool pvm_access_id_aarch32(struct kvm_vcpu *vcpu,
 	 * of AArch32 feature id registers.
 	 */
 	BUILD_BUG_ON(FIELD_GET(ARM64_FEATURE_MASK(ID_AA64PFR0_EL1_EL1),
-		     PVM_ID_AA64PFR0_RESTRICT_UNSIGNED) > ID_AA64PFR0_EL1_ELx_64BIT_ONLY);
+		     PVM_ID_AA64PFR0_ALLOW) > ID_AA64PFR0_EL1_ELx_64BIT_ONLY);
 
 	return pvm_access_raz_wi(vcpu, p, r);
 }

hlyytine/linux

arch/arm64/kvm/hyp/nvhe/sys_regs.c
@@ -52,7 +52,6 @@ static void inject_undef64(struct kvm_vcpu *vcpu)
 /*
  * Returns the restricted features values of the feature register based on the
  * limitations in restrict_fields.
- * A feature id field value of 0b0000 does not impose any restrictions.
  * Note: Use only for unsigned feature field values.
  */
 static u64 get_restricted_features_unsigned(u64 sys_reg_val,
@@ -85,24 +84,20 @@ static u64 get_restricted_features_unsigned(u64 sys_reg_val,
 
 static u64 get_pvm_id_aa64pfr0(const struct kvm_vcpu *vcpu)
 {
-	u64 set_mask = 0;
-	u64 allow_mask = PVM_ID_AA64PFR0_ALLOW;
-
-	set_mask |= get_restricted_features_unsigned(id_aa64pfr0_el1_sys_val,
-		PVM_ID_AA64PFR0_RESTRICT_UNSIGNED);
-
-	return (id_aa64pfr0_el1_sys_val & allow_mask) | set_mask;
+	return get_restricted_features_unsigned(id_aa64pfr0_el1_sys_val,
+						PVM_ID_AA64PFR0_ALLOW);
 }
 
 static u64 get_pvm_id_aa64pfr1(const struct kvm_vcpu *vcpu)
 {
 	const struct kvm *kvm = (const struct kvm *)kern_hyp_va(vcpu->kvm);
-	u64 allow_mask = PVM_ID_AA64PFR1_ALLOW;
+	u64 value = get_restricted_features_unsigned(id_aa64pfr1_el1_sys_val,
+						     PVM_ID_AA64PFR1_ALLOW);
 
 	if (!kvm_has_mte(kvm))
-		allow_mask &= ~ARM64_FEATURE_MASK(ID_AA64PFR1_EL1_MTE);
+		value &= ~ARM64_FEATURE_MASK(ID_AA64PFR1_EL1_MTE);
 
-	return id_aa64pfr1_el1_sys_val & allow_mask;
+	return value;
 }
 
 static u64 get_pvm_id_aa64zfr0(const struct kvm_vcpu *vcpu)
@@ -162,46 +157,46 @@ static u64 get_pvm_id_aa64isar0(const struct kvm_vcpu *vcpu)
 
 static u64 get_pvm_id_aa64isar1(const struct kvm_vcpu *vcpu)
 {
-	u64 allow_mask = PVM_ID_AA64ISAR1_ALLOW;
+	u64 value = get_restricted_features_unsigned(id_aa64isar1_el1_sys_val,
+						     PVM_ID_AA64ISAR1_ALLOW);
 
 	if (!vcpu_has_ptrauth(vcpu))
-		allow_mask &= ~(ARM64_FEATURE_MASK(ID_AA64ISAR1_EL1_APA) |
-				ARM64_FEATURE_MASK(ID_AA64ISAR1_EL1_API) |
-				ARM64_FEATURE_MASK(ID_AA64ISAR1_EL1_GPA) |
-				ARM64_FEATURE_MASK(ID_AA64ISAR1_EL1_GPI));
+		value &= ~(ARM64_FEATURE_MASK(ID_AA64ISAR1_EL1_APA) |
+			   ARM64_FEATURE_MASK(ID_AA64ISAR1_EL1_API) |
+			   ARM64_FEATURE_MASK(ID_AA64ISAR1_EL1_GPA) |
+			   ARM64_FEATURE_MASK(ID_AA64ISAR1_EL1_GPI));
 
-	return id_aa64isar1_el1_sys_val & allow_mask;
+	return value;
 }
 
 static u64 get_pvm_id_aa64isar2(const struct kvm_vcpu *vcpu)
 {
-	u64 allow_mask = PVM_ID_AA64ISAR2_ALLOW;
+	u64 value = get_restricted_features_unsigned(id_aa64isar2_el1_sys_val,
+						     PVM_ID_AA64ISAR2_ALLOW);
 
 	if (!vcpu_has_ptrauth(vcpu))
-		allow_mask &= ~(ARM64_FEATURE_MASK(ID_AA64ISAR2_EL1_APA3) |
-				ARM64_FEATURE_MASK(ID_AA64ISAR2_EL1_GPA3));
+		value &= ~(ARM64_FEATURE_MASK(ID_AA64ISAR2_EL1_APA3) |
+			   ARM64_FEATURE_MASK(ID_AA64ISAR2_EL1_GPA3));
 
-	return id_aa64isar2_el1_sys_val & allow_mask;
+	return id_aa64isar2_el1_sys_val & value;
 }
 
 static u64 get_pvm_id_aa64mmfr0(const struct kvm_vcpu *vcpu)
 {
-	u64 set_mask;
-
-	set_mask = get_restricted_features_unsigned(id_aa64mmfr0_el1_sys_val,
-		PVM_ID_AA64MMFR0_RESTRICT_UNSIGNED);
-
-	return (id_aa64mmfr0_el1_sys_val & PVM_ID_AA64MMFR0_ALLOW) | set_mask;
+	return get_restricted_features_unsigned(id_aa64mmfr0_el1_sys_val,
+						PVM_ID_AA64MMFR0_ALLOW);
 }
 
 static u64 get_pvm_id_aa64mmfr1(const struct kvm_vcpu *vcpu)
 {
-	return id_aa64mmfr1_el1_sys_val & PVM_ID_AA64MMFR1_ALLOW;
+	return get_restricted_features_unsigned(id_aa64mmfr1_el1_sys_val,
+						PVM_ID_AA64MMFR1_ALLOW);
 }
 
 static u64 get_pvm_id_aa64mmfr2(const struct kvm_vcpu *vcpu)
 {
-	return id_aa64mmfr2_el1_sys_val & PVM_ID_AA64MMFR2_ALLOW;
+	return get_restricted_features_unsigned(id_aa64mmfr2_el1_sys_val,
+						PVM_ID_AA64MMFR2_ALLOW);
 }
 
 /* Read a sanitized cpufeature ID register by its encoding */
@@ -276,7 +271,7 @@ static bool pvm_access_id_aarch32(struct kvm_vcpu *vcpu,
 	 * of AArch32 feature id registers.
 	 */
 	BUILD_BUG_ON(FIELD_GET(ARM64_FEATURE_MASK(ID_AA64PFR0_EL1_EL1),
-		     PVM_ID_AA64PFR0_RESTRICT_UNSIGNED) > ID_AA64PFR0_EL1_ELx_64BIT_ONLY);
+		     PVM_ID_AA64PFR0_ALLOW) > ID_AA64PFR0_EL1_ELx_64BIT_ONLY);
 
 	return pvm_access_raz_wi(vcpu, p, r);
 }