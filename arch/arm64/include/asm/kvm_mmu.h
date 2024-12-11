06a9cd1e644b2d6f43f0cb52457130c7373102de
Sebastian Ene <sebastianene@google.com>
2024-12-11 17:50:05 +0000

    ANDROID: KVM: arm64: Add host stage-2 ptdump support
    Allow the host stage-2 pagetables to be dumped to
    debugfs using ptdump. When pKVM is enabled, register
    new virtual file entries with debugfs and use the
    previously introduced HVC(__pkvm_ptdump_handle) to
    walk the host stage-2 pagetables and print the
    descriptors to a shared buffer.
    
    Bug: 337041793
    Bug: 353523878
    Bug: 357781595
    Change-Id: Ib28e18a9a466c8c91850cda2922a12305f722e46
    Signed-off-by: Sebastian Ene <sebastianene@google.com>

--- arch/arm64/include/asm/kvm_mmu.h
+++ arch/arm64/include/asm/kvm_mmu.h
@@ -382,8 +382,10 @@ static inline void kvm_fault_unlock(struct kvm *kvm)
 
 #ifdef CONFIG_PTDUMP_STAGE2_DEBUGFS
 void kvm_s2_ptdump_create_debugfs(struct kvm *kvm);
+void kvm_s2_ptdump_host_create_debugfs(void);
 #else
 static inline void kvm_s2_ptdump_create_debugfs(struct kvm *kvm) {}
+static inline void kvm_s2_ptdump_host_create_debugfs(void) {}
 #endif /* CONFIG_PTDUMP_STAGE2_DEBUGFS */
 
 #endif /* __ASSEMBLY__ */
