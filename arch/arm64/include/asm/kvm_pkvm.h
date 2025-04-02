c3d8db72cb95e70c2b4b45d2b1c5c02e9fb9d44a
Fuad Tabba <tabba@google.com>
2025-04-02 16:53:32 +0000

    ANDROID: KVM: arm64: Check supported VCPU features for protected VMs
    Check at the host kernel which VCPU features are supported for pKVM. The
    hypervisor performs a similar check, however, an unsupported feature
    could be disabled silently. Ensure that userspace is aware if it tries
    to set an unsupported feature for a protected VCPU.
    
    Also, ensure that hyp and the host share the same code for checking
    supported features for protected VMs.
    
    This patch also fixes a bug where it was potentially setting
    KVM_CAP_ARM_MTE, a VM capability, as a VCPU feature.
    
    Bug: 357781595
    Bug: 407526209
    
    Change-Id: Ice3e0364b13d4d1ad78926c62e38d35f744b7408
    Signed-off-by: Fuad Tabba <tabba@google.com>

--- arch/arm64/include/asm/kvm_pkvm.h
+++ arch/arm64/include/asm/kvm_pkvm.h
@@ -51,6 +51,33 @@ static inline bool kvm_pvm_ext_allowed(long ext)
 	}
 }
 
+static inline unsigned long pvm_supported_vcpu_features(void)
+{
+	unsigned long features = 0;
+
+	set_bit(KVM_ARM_VCPU_POWER_OFF, &features);
+
+	if (kvm_pvm_ext_allowed(KVM_CAP_ARM_EL1_32BIT))
+		set_bit(KVM_ARM_VCPU_EL1_32BIT, &features);
+
+	if (kvm_pvm_ext_allowed(KVM_CAP_ARM_PSCI_0_2))
+		set_bit(KVM_ARM_VCPU_PSCI_0_2, &features);
+
+	if (kvm_pvm_ext_allowed(KVM_CAP_ARM_PMU_V3))
+		set_bit(KVM_ARM_VCPU_PMU_V3, &features);
+
+	if (kvm_pvm_ext_allowed(KVM_CAP_ARM_SVE))
+		set_bit(KVM_ARM_VCPU_SVE, &features);
+
+	if (kvm_pvm_ext_allowed(KVM_CAP_ARM_PTRAUTH_ADDRESS) &&
+	    kvm_pvm_ext_allowed(KVM_CAP_ARM_PTRAUTH_GENERIC)) {
+		set_bit(KVM_ARM_VCPU_PTRAUTH_ADDRESS, &features);
+		set_bit(KVM_ARM_VCPU_PTRAUTH_GENERIC, &features);
+	}
+
+	return features;
+}
+
 extern struct memblock_region kvm_nvhe_sym(hyp_memory)[];
 extern unsigned int kvm_nvhe_sym(hyp_memblock_nr);
 
