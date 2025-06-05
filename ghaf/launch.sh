f39ce5bdd705b761a2972f1a0bceb63a8fcc47a0
Hannu Lyytinen <hannu.lyytinen@unikie.com>
2025-06-05 10:31:20 +0300

    Revert to simplified gpuvm start scripts
    Signed-off-by: Hannu Lyytinen <hannu.lyytinen@unikie.com>

--- ghaf/launch.sh
+++ ghaf/launch.sh
@@ -1,21 +1,13 @@
 #! /bin/sh
 
 if test "x${DTB}" = "x"; then
-  DTB=tegra234-qemu-4cpus-4gb.dtb
+  DTB=tegra234-gpuvm.dtb
 fi
 
-EARLYCON="earlycon=pl011,0x9000000,115200n8"
+EARLYCON=""
+#EARLYCON="earlycon=pl011,0x9000000,115200n8"
 
-QEMU_PASSTHROUGH_DEVICES=""
-PASSTHROUGH_DEVICES=""
-
-. ./passthrough_devices.sh
-
-for dev_mmiobase in ${PASSTHROUGH_DEVICES}; do
-	QEMU_PASSTHROUGH_DEVICES="${QEMU_PASSTHROUGH_DEVICES} -device vfio-platform,host=${dev_mmiobase}"
-done
-
-exec /home/ubuntu/qemu/build/qemu-system-aarch64 \
+exec /home/ubuntu/qemu/build/qemu-system-aarch64.wrapper \
 	`# minicom already uses ^A, change monitor escape to ^T` \
 	-echr 0x14 \
 	-nographic \
@@ -25,19 +17,25 @@ exec /home/ubuntu/qemu/build/qemu-system-aarch64 \
 	-m 6000 \
 	-no-reboot \
 	-kernel /home/ubuntu/Image \
+	-dtb ${DTB} \
 	-drive file=rootfs-guest.qcow2,if=none,id=drive0,format=qcow2 \
         -device virtio-blk-pci,drive=drive0,packed=on \
-	`# -machine dumpdtb=qemu.dtb` \
-	`# -dtb qemu.dtb`  \
-	-dtb ${DTB} \
-	-append "rootwait root=/dev/vda console=hvc0 "${EARLYCON}" clk_ignore_unused pd_ignore_unused" \
+        -netdev user,id=net0,hostfwd=tcp::2222-:22 \
+        -device virtio-net-pci,netdev=net0 \
+	-append "rootwait root=/dev/vda quiet loglevel=2 "${EARLYCON}" clk_ignore_unused pd_ignore_unused" \
 	`# enable multiplexer on stdio to have both guest and monitor` \
 	-chardev stdio,id=mon,mux=on,signal=off \
 	`# enable monitor` \
 	-mon chardev=mon,mode=readline \
 	-serial chardev:mon \
-	`# virtio-console` \
-	-device virtio-serial-pci \
-	-device virtconsole,chardev=mon,id=console0,name=guestvm \
 	`# passthrough devices` \
-	${QEMU_PASSTHROUGH_DEVICES}
+	-device vfio-platform,host=70000000.vm_hs_p,mmio-base=0x70000000 \
+	-device vfio-platform,host=80000000.vm_cma_p,mmio-base=0x80000000 \
+	-device vfio-platform,host=100000000.vm_cma_vram_p,mmio-base=0x100000000 \
+	-device vfio-platform,host=17000000.gpu \
+	-device vfio-platform,host=13e00000.host1x_pt \
+	-device vfio-platform,host=15340000.vic \
+	-device vfio-platform,host=15480000.nvdec \
+	-device vfio-platform,host=15540000.nvjpg \
+	-device vfio-platform,host=d800000.dce \
+	-device vfio-platform,host=13800000.display
