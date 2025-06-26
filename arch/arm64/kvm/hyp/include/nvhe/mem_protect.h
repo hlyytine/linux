BayLibre/android_kernel_common

ANDROID: KVM: arm64: Don't always update IOMMUs from module_change_host_page_prot
Host stage-2 changes initiated from module may not need to trigger IOMMU
updates as DMA attacks is out of scope for their threat model. As such,
don't bother calling into the IOMMU layer from
module_change_host_page_prot if not requested by the module.

While at it, make sure to trigger an IOMMU update during MMIO
donations in the normal set_owner_locked() path.

Bug: 278749606
Bug: 322736772
Change-Id: Idaeecb3983d9d1ee753845ec2b764d32c762f35a
Signed-off-by: Quentin Perret <qperret@google.com>

arch/arm64/kvm/hyp/include/nvhe/mem_protect.h
@@ -82,7 +82,7 @@ int reclaim_hyp_pool(struct hyp_pool *pool, struct kvm_hyp_memcache *host_mc,
 void destroy_hyp_vm_pgt(struct pkvm_hyp_vm *vm);
 void drain_hyp_pool(struct pkvm_hyp_vm *vm, struct kvm_hyp_memcache *mc);
 
-int module_change_host_page_prot(u64 pfn, enum kvm_pgtable_prot prot, u64 nr_pages);
+int module_change_host_page_prot(u64 pfn, enum kvm_pgtable_prot prot, u64 nr_pages, bool update_mmio);
 
 void psci_mem_protect_inc(u64 n);
 void psci_mem_protect_dec(u64 n);

hlyytine/linux

arch/arm64/kvm/hyp/include/nvhe/mem_protect.h
@@ -90,7 +90,7 @@ int reclaim_hyp_pool(struct hyp_pool *pool, struct kvm_hyp_memcache *host_mc,
 void destroy_hyp_vm_pgt(struct pkvm_hyp_vm *vm);
 void drain_hyp_pool(struct pkvm_hyp_vm *vm, struct kvm_hyp_memcache *mc);
 
-int module_change_host_page_prot(u64 pfn, enum kvm_pgtable_prot prot, u64 nr_pages);
+int module_change_host_page_prot(u64 pfn, enum kvm_pgtable_prot prot, u64 nr_pages, bool update_iommu);
 
 void psci_mem_protect_inc(u64 n);
 void psci_mem_protect_dec(u64 n);