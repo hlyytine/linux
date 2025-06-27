BayLibre/android_kernel_common

ANDROID: KVM: arm64: Export topup_hyp_memcache
Export topup_hyp_memcache so we can use when the driver is compiled
as a module.

Bug: 277989609
Bug: 278749606
Change-Id: Ia98a563675ef7fa6e86f0e645ad377fe5705ca61

arch/arm64/kvm/mmu.c
@@ -1110,6 +1110,7 @@ int topup_hyp_memcache(struct kvm_hyp_memcache *mc, unsigned long min_pages,
 	return __topup_hyp_memcache(mc, min_pages, hyp_mc_alloc_fn,
 				    kvm_host_pa, (void *)flags, order);
 }
+EXPORT_SYMBOL_GPL(topup_hyp_memcache);
 
 /**
  * kvm_phys_addr_ioremap - map a device range to guest IPA

hlyytine/linux

Bug: 357781595
Bug: 384432312
Signed-off-by: Mostafa Saleh <smostafa@google.com>

arch/arm64/kvm/mmu.c
@@ -1262,6 +1262,7 @@ int topup_hyp_memcache(struct kvm_hyp_memcache *mc, unsigned long min_pages,
 	return __topup_hyp_memcache(mc, min_pages, hyp_mc_alloc_fn,
 				    kvm_host_pa, (void *)flags, order);
 }
+EXPORT_SYMBOL(topup_hyp_memcache);
 
 int topup_hyp_memcache_gfp(struct kvm_hyp_memcache *mc, unsigned long min_pages,
 			   unsigned long order, gfp_t gfp)