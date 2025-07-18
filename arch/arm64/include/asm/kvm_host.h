33f74d010a4834a82d7744ce7a3e25d6fea99fb9
Fuad Tabba <tabba@google.com>
2025-07-18 11:26:34 +0100

    ANDROID: KVM: arm64: Consolidate teardown_mc and stage2_teardown_mc
    These two should be the same memcache, but due to how things ended up
    upstream, we have both.
    
    Change-Id: If53dc680ef94df045637a6e4990133b13ffddbe4
    Signed-off-by: Fuad Tabba <tabba@google.com>

--- arch/arm64/include/asm/kvm_host.h
+++ arch/arm64/include/asm/kvm_host.h
@@ -249,7 +249,6 @@ typedef unsigned int pkvm_handle_t;
 
 struct kvm_protected_vm {
 	pkvm_handle_t handle;
-	struct kvm_hyp_memcache teardown_mc;
 	struct kvm_hyp_memcache stage2_teardown_mc;
 	gpa_t pvmfw_load_addr;
 	bool enabled;
