803fc8eb1737b4b84c30fea00383b99655ed9e75
Quentin Perret <qperret@google.com>
2025-07-21 12:07:26 +0000

    ANDROID: KVM: arm64: Fix __pkvm_reclaim_dying_guest_page()
    Change-Id: Iadef995695cf9b1628335e1cb1b46b3defc523d5
    Signed-off-by: Quentin Perret <qperret@google.com>

--- arch/arm64/kvm/hyp/nvhe/hyp-main.c
+++ arch/arm64/kvm/hyp/nvhe/hyp-main.c
@@ -1425,7 +1425,7 @@ static void handle___pkvm_init_vcpu(struct kvm_cpu_context *host_ctxt)
 static void handle___pkvm_reclaim_dying_guest_page(struct kvm_cpu_context *host_ctxt)
 {
 	DECLARE_REG(pkvm_handle_t, handle, host_ctxt, 1);
-	DECLARE_REG(u64, gfn, host_ctxt, 3);
+	DECLARE_REG(u64, gfn, host_ctxt, 2);
 
 	cpu_reg(host_ctxt, 1) = __pkvm_reclaim_dying_guest_page(handle, gfn);
 }
