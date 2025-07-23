62c6f191c9fcfdbbc8d3e5c93f1cfece00912974
Fuad Tabba <tabba@google.com>
2025-07-23 12:38:53 +0100

    ANDROID: KVM: arm64: Assign memcache if topping up
    Change-Id: I276e0f32f26ee925498fc59f96928688ad31421c
    Signed-off-by: Fuad Tabba <tabba@google.com>

--- arch/arm64/kvm/mmu.c
+++ arch/arm64/kvm/mmu.c
@@ -1898,11 +1898,6 @@ static int user_mem_abort(struct kvm_vcpu *vcpu, phys_addr_t fault_ipa,
 		return -EFAULT;
 	}
 
-	if (!is_protected_kvm_enabled())
-		memcache = &vcpu->arch.mmu_page_cache;
-	else
-		memcache = &vcpu->arch.stage2_mc;
-
 	/*
 	 * Permission faults just need to update the existing leaf entry,
 	 * and so normally don't require allocations from the memcache. The
@@ -1912,10 +1907,13 @@ static int user_mem_abort(struct kvm_vcpu *vcpu, phys_addr_t fault_ipa,
 	if (!fault_is_perm || (logging_active && write_fault)) {
 		int min_pages = kvm_mmu_cache_min_pages(vcpu->arch.hw_mmu);
 
-		if (!is_protected_kvm_enabled())
+		if (!is_protected_kvm_enabled()) {
+			memcache = &vcpu->arch.mmu_page_cache;
 			ret = kvm_mmu_topup_memory_cache(memcache, min_pages);
-		else
+		} else {
+			memcache = &vcpu->arch.stage2_mc;
 			ret = topup_hyp_memcache(memcache, min_pages, 0);
+		}
 		if (ret)
 			return ret;
 	}
