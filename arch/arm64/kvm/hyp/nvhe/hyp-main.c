BayLibre/android_kernel_common

ANDROID: KVM: arm64: Add a range to pkvm_host_map_guest
In preparation for support guest stage-2 block mappings, allow the host
to map an entire contiguous range of physically contiguous memory.

Bug: 278749606
Bug: 278011447
Change-Id: Id5704d958eab94a4def15353999df26a597b70b3
Signed-off-by: Vincent Donnefort <vdonnefort@google.com>

arch/arm64/kvm/hyp/nvhe/hyp-main.c
@@ -989,7 +989,8 @@ static void handle___pkvm_host_map_guest(struct kvm_cpu_context *host_ctxt)
 {
 	DECLARE_REG(u64, pfn, host_ctxt, 1);
 	DECLARE_REG(u64, gfn, host_ctxt, 2);
-	DECLARE_REG(enum kvm_pgtable_prot, prot, host_ctxt, 3);
+	DECLARE_REG(u64, nr_pages, host_ctxt, 3);
+	DECLARE_REG(enum kvm_pgtable_prot, prot, host_ctxt, 4);
 	struct pkvm_hyp_vcpu *hyp_vcpu;
 	int ret = -EINVAL;
 
@@ -1006,9 +1007,9 @@ static void handle___pkvm_host_map_guest(struct kvm_cpu_context *host_ctxt)
 		goto out;
 
 	if (pkvm_hyp_vcpu_is_protected(hyp_vcpu))
-		ret = __pkvm_host_donate_guest(pfn, gfn, hyp_vcpu);
+		ret = __pkvm_host_donate_guest(hyp_vcpu, pfn, gfn, nr_pages);
 	else
-		ret = __pkvm_host_share_guest(pfn, gfn, hyp_vcpu, prot);
+		ret = __pkvm_host_share_guest(hyp_vcpu, pfn, gfn, nr_pages, prot);
 out:
 	cpu_reg(host_ctxt, 1) =  ret;
 }

hlyytine/linux

Bug: 357781595

arch/arm64/kvm/hyp/nvhe/hyp-main.c
@@ -1030,7 +1030,8 @@ static void handle___pkvm_host_map_guest(struct kvm_cpu_context *host_ctxt)
 {
 	DECLARE_REG(u64, pfn, host_ctxt, 1);
 	DECLARE_REG(u64, gfn, host_ctxt, 2);
-	DECLARE_REG(enum kvm_pgtable_prot, prot, host_ctxt, 3);
+	DECLARE_REG(u64, nr_pages, host_ctxt, 3);
+	DECLARE_REG(enum kvm_pgtable_prot, prot, host_ctxt, 4);
 	struct pkvm_hyp_vcpu *hyp_vcpu;
 	int ret = -EINVAL;
 
@@ -1046,9 +1047,9 @@ static void handle___pkvm_host_map_guest(struct kvm_cpu_context *host_ctxt)
 		goto out;
 
 	if (pkvm_hyp_vcpu_is_protected(hyp_vcpu))
-		ret = __pkvm_host_donate_guest(pfn, gfn, hyp_vcpu);
+		ret = __pkvm_host_donate_guest(pfn, gfn, hyp_vcpu, nr_pages);
 	else
-		ret = __pkvm_host_share_guest(pfn, gfn, hyp_vcpu, prot);
+		ret = __pkvm_host_share_guest(pfn, gfn, hyp_vcpu, nr_pages, prot);
 out:
 	cpu_reg(host_ctxt, 1) =  ret;
 }