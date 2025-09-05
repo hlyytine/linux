0e03c900d1e7fb8ae917b2c0990f8f3327e1bfef
Fuad Tabba <tabba@google.com>
2025-09-05 09:04:35 +0100

    ANDROID: KVM: arm64: Use stored host arch flags
    Host data should be read once, so use the stored flags when accessing
    them.
    
    Fixes: b7f345fbc32a ("KVM: arm64: Fix FEAT_MTE in pKVM")
    
    Change-Id: Id6d158aeb8b74cece5045248d459b7df768093e3
    Signed-off-by: Fuad Tabba <tabba@google.com>

--- arch/arm64/kvm/hyp/nvhe/pkvm.c
+++ arch/arm64/kvm/hyp/nvhe/pkvm.c
@@ -355,9 +355,6 @@ static void pkvm_init_features_from_host(struct pkvm_hyp_vm *hyp_vm, const struc
 	/* CTR_EL0 is always under host control, even for protected VMs. */
 	hyp_vm->kvm.arch.ctr_el0 = host_kvm->arch.ctr_el0;
 
-	if (test_bit(KVM_ARCH_FLAG_MTE_ENABLED, &host_kvm->arch.flags))
-		set_bit(KVM_ARCH_FLAG_MTE_ENABLED, &kvm->arch.flags);
-
 	/* No restrictions for non-protected VMs. */
 	if (!kvm_vm_is_protected(kvm)) {
 		hyp_vm->kvm.arch.flags = host_arch_flags;
@@ -374,6 +371,9 @@ static void pkvm_init_features_from_host(struct pkvm_hyp_vm *hyp_vm, const struc
 
 	kvm->arch.vcpu_features[0] = pvm_supported_vcpu_features() &
 				     host_kvm->arch.vcpu_features[0];
+
+	if (test_bit(KVM_ARCH_FLAG_MTE_ENABLED, &host_arch_flags))
+		set_bit(KVM_ARCH_FLAG_MTE_ENABLED, &kvm->arch.flags);
 
 	if (kvm_pvm_ext_allowed(KVM_CAP_ARM_SVE) && kvm_has_sve(host_kvm))
 		set_bit(KVM_ARCH_FLAG_GUEST_HAS_SVE, &kvm->arch.flags);
