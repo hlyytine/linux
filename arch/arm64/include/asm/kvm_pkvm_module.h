maltesermailo/android_kernel_pantah

ANDROID: KVM: arm64: Allow handling illegal aborts from pKVM modules
Introduce a new handler allowing to notify pKVM modules when pKVM
detects an illegal access from the host.

Bug: 244543039
Bug: 245034629
Change-Id: I62133a8d967d91437e5216b307e449f8c83dfab6
Signed-off-by: Quentin Perret <qperret@google.com>

arch/arm64/include/asm/kvm_pkvm_module.h
@@ -21,6 +21,7 @@ struct pkvm_module_ops {
 	int (*register_host_perm_fault_handler)(int (*cb)(struct kvm_cpu_context *ctxt, u64 esr, u64 addr));
 	int (*protect_host_page)(u64 pfn, enum kvm_pgtable_prot prot);
 	int (*register_host_smc_handler)(bool (*cb)(struct kvm_cpu_context *));
+	int (*register_illegal_abt_notifier)(void (*cb)(struct kvm_cpu_context *));
 };
 
 struct pkvm_module_section {

arch/arm64/kvm/hyp/include/nvhe/modules.h
@@ -4,6 +4,7 @@
 #define HCALL_UNHANDLED -1
 
 int __pkvm_register_host_smc_handler(bool (*cb)(struct kvm_cpu_context *));
+int __pkvm_register_illegal_abt_notifier(void (*cb)(struct kvm_cpu_context *));
 
 #ifdef CONFIG_MODULES
 int __pkvm_init_module(void *module_init);

hlyytine/linux

Bug: 357781595

arch/arm64/include/asm/kvm_pkvm_module.h
@@ -22,6 +22,7 @@ struct pkvm_module_ops {
 	void (*flush_dcache_to_poc)(void *addr, size_t size);
 	int (*register_host_perm_fault_handler)(int (*cb)(struct user_pt_regs *regs, u64 esr, u64 addr));
 	int (*register_host_smc_handler)(bool (*cb)(struct user_pt_regs *));
+	int (*register_illegal_abt_notifier)(void (*cb)(struct user_pt_regs *));
 };
 
 int __pkvm_load_el2_module(struct module *this, unsigned long *token);

arch/arm64/kvm/hyp/include/nvhe/modules.h
@@ -4,6 +4,7 @@
 #define HCALL_UNHANDLED -1
 
 int __pkvm_register_host_smc_handler(bool (*cb)(struct user_pt_regs *));
+int __pkvm_register_illegal_abt_notifier(void (*cb)(struct user_pt_regs *));
 
 #ifdef CONFIG_MODULES
 int __pkvm_init_module(void *module_init);